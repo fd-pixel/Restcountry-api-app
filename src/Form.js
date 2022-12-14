461c23e382e10dd779896363389d2d76b7c08be9
FikretDuzceker <fikretduzceker5@gmail.com>
2022-12-14 09:52:51 +0300

    Filter function fixed

--- src/Form.js
+++ src/Form.js
@@ -6,10 +6,12 @@ import Country from "./Country";
 const Form = () => {
   const [countryList, setCountryList] = useState([]);
   const [input, setInput] = useState("");
-
-  const url2 = `https://restcountries.com/v2/name/${input}`;
+  const [input2, setInput2] = useState("");
 
   const url1 = "https://restcountries.com/v2/all";
+  const url2 = `https://restcountries.com/v2/name/${input}`;
+  const url3 = `https://restcountries.com/v2/region/${input2}`;
+
   const getAllCountries = async () => {
     const res = await axios.get(url1);
     console.log(res.data);
@@ -20,7 +22,11 @@ const Form = () => {
     console.log(res.data);
     setCountryList(res.data);
   };
-
+  const getCountryByRegion = async (input2) => {
+    const res = await axios.get(url3);
+    console.log(res.data);
+    setCountryList(res.data);
+  };
   useEffect(() => {
     getAllCountries();
   }, []);
@@ -30,11 +36,18 @@ const Form = () => {
     //console.log({ region });
     getCountryByName(input);
   };
+  const handleSearch2 = (e) => {
+    e.preventDefault();
+    //console.log({ region });
+
+    getCountryByRegion(input2);
+  };
   return (
     <div>
       <div className="form">
         <form onSubmit={handleSearch} action="">
           <input
+            className="input"
             type="text"
             onChange={(e) => setInput(e.target.value.toLowerCase())}
             id="input"
@@ -43,7 +56,12 @@ const Form = () => {
           <button type="submit" class="btn btn-primary btn-sm">
             Search by Name
           </button>
-          <select className="hero__select">
+        </form>
+        <form onSubmit={handleSearch2} action="">
+          <select
+            className="input"
+            onChange={(e) => setInput2(e.target.value.toLowerCase())}
+          >
             <option value="all">Filter by Region</option>
             <option value="africa">Africa</option>
             <option value="americas">America</option>
@@ -57,9 +75,13 @@ const Form = () => {
         </form>
       </div>
       <div className="main">
-        {countryList.map((country, index) => (
-          <Country country={country} key={index} />
-        ))}
+        {countryList
+          .filter((country) =>
+            country.name.toLocaleLowerCase().includes(input.toLocaleLowerCase())
+          )
+          .map((country, index) => (
+            <Country country={country} key={index} />
+          ))}
       </div>
     </div>
   );
